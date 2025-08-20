8153bdb642030cdc5f03ab600bf2aee39fba7ce3
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-08-21 00:01:03 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import SharedLayout from "./SharedLayout/SharedLayout.jsx";
 const HomePage = lazy(() => import("../pages/HomePage/HomePage.jsx"));
 
@@ -20,55 +20,60 @@ export default function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SharedLayout />}>
-            <Route
-              index
-              element={
-                <HomePage toggleLoading={setLoading} toggleError={setError} />
-              }
-            />
-            <Route
-              path="/movies"
-              element={
-                <MoviesPage toggleLoading={setLoading} toggleError={setError} />
-              }
-            />
-            <Route
-              path="/movies/:movieId"
-              element={
-                <MovieDetailsPage
-                  toggleLoading={setLoading}
-                  toggleError={setError}
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SharedLayout />}>
+              <Route
+                index
+                element={
+                  <HomePage toggleLoading={setLoading} toggleError={setError} />
+                }
+              />
+              <Route
+                path="/movies"
+                element={
+                  <MoviesPage
+                    toggleLoading={setLoading}
+                    toggleError={setError}
+                  />
+                }
+              />
+              <Route
+                path="/movies/:movieId"
+                element={
+                  <MovieDetailsPage
+                    toggleLoading={setLoading}
+                    toggleError={setError}
+                  />
+                }
+              >
+                <Route
+                  path="cast"
+                  element={
+                    <MovieCast
+                      toggleLoading={setLoading}
+                      toggleError={setError}
+                    />
+                  }
                 />
-              }
-            >
-              <Route
-                path="cast"
-                element={
-                  <MovieCast
-                    toggleLoading={setLoading}
-                    toggleError={setError}
-                  />
-                }
-              />
-              <Route
-                path="reviews"
-                element={
-                  <MovieReviews
-                    toggleLoading={setLoading}
-                    toggleError={setError}
-                  />
-                }
-              />
+                <Route
+                  path="reviews"
+                  element={
+                    <MovieReviews
+                      toggleLoading={setLoading}
+                      toggleError={setError}
+                    />
+                  }
+                />
+              </Route>
             </Route>
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      {loading && <Loader />}
-      {error && <Error />}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+        {loading && <Loader />}
+        {error && <Error />}
+      </Suspense>
     </>
   );
 }
